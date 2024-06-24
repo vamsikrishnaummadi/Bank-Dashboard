180d5d24db84c583c0c08a0184e849133a89d80b
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-06-24 17:13:58 +0530

    update delete notification code

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -23,3 +23,18 @@ export const getNotifications = async (req, res, next) => {
         next(errorHandler(500, "Failed to retrieve notifications"));
     }
 };
+
+
+export const deleteNotification = async(req,res,next) =>{
+    const {notificationId} = req.params; 
+
+    try{
+        const notification = await Notification.findByIdAndDelete(notificationId); 
+        if(!notification){
+            return next(errorHandler(404, "Notification not found"));
+        }
+        res.status(200).send({message: "Notification deleted successfully"})
+    } catch(error){
+        next(errorHandler(500, "Failed to delete notification"));
+    }
+}
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,8 +1,8 @@
-import { createNotification, getNotifications } from '../controllers/notification.controller';
+import { createNotification, getNotifications, deleteNotification } from '../controllers/notification.controller';
 const router = express.Router();
 
 router.post('/', createNotification); 
 router.get('/:userId', getNotifications);
-
+router.delete('/:notificationId', deleteNotification);
 export default router;
 
