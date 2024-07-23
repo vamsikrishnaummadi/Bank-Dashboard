441e5603a7970ce445c2eb6a74339e68ff52cc2a
vamsikrishnaummadi <krishnavamsi9432@gmail.com>
2024-07-23 12:07:50 +0530

    investment page

--- frontend/src/components/Investments.tsx
+++ frontend/src/components/Investments.tsx
@@ -1,5 +1,39 @@
+import {totalInvestmentIcon, numberOfInvestmentIcon, rateOfReturnIcon} from "../utils/assetsGroup";
+
 const Investments = () => {
-  return <div></div>;
+  return (
+       <div className="rounded-lg flex flex-col justify-center">
+            <div className="flex items-center justify-center gap-5 w-full flex-wrap">
+                  <div className="flex items-center gap-4 bg-white rounded-lg shadow-md p-4 w-full sm:w-56 md:w-60 lg:w-72">
+                        <img src={totalInvestmentIcon} alt="total investment icon" />
+                        <div className="flex flex-col justify-start">
+                           <span>Total Invested Amount</span>
+                           <p>$150,000</p>
+                        </div>
+                  </div>
+                  <div className="flex items-center gap-4 bg-white rounded-lg shadow-md p-4 w-full sm:w-56 md:w-60 lg:w-72">
+                        <img src={numberOfInvestmentIcon} alt="total investment icon" />
+                        <div className="flex flex-col justify-start">
+                           <span>Number of Investments</span>
+                           <p>1,250</p>
+                        </div>
+                  </div>
+                  <div className="flex items-center gap-4 bg-white rounded-lg shadow-md p-4 w-full sm:w-56 md:w-60 lg:w-72">
+                        <img src={rateOfReturnIcon} alt="total investment icon" />
+                        <div className="flex flex-col justify-start">
+                           <span>Rate of Return</span>
+                           <p>5.8%</p>
+                        </div>
+                  </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+       </div>
+  )
 };
 
 export default Investments;
---
+++ frontend/src/utils/assetsGroup.ts
@@ -0,0 +1,12 @@
+import avatar from "../assets/avatar.png";
+import hamburgerIcon from "../assets/hamburger.svg";
+import logo from "../assets/Logo.svg";
+import notificationBellIcon from "../assets/notificationBell.svg";
+import searchIcon from "../assets/searchicon.svg";
+import numberOfInvestmentIcon from "../assets/number-of-investment.svg";
+import rateOfReturnIcon from "../assets/rate-of-return.svg";
+import totalInvestmentIcon from "../assets/total-investment.svg";
+import settingsIcon from "../assets/settings.svg";
+
+
+export {avatar, hamburgerIcon, logo, notificationBellIcon, searchIcon, numberOfInvestmentIcon, rateOfReturnIcon, totalInvestmentIcon, settingsIcon};
