dba588f99e62f58aa8c7c4a77079c59e0799df47
Dasari Prashanth <prashanth60238@gmail.com>
2024-09-20 17:22:07 +0530

    fix: Typescript errors

--- frontend/src/components/Header/BellIcon.tsx
+++ frontend/src/components/Header/BellIcon.tsx
@@ -1,85 +1,107 @@
-import React, { useState, useEffect} from "react";
-import { headerNotificationBellIcon, notificationCredit, notificationDebit, notificationFailed, notificationGrant, notificationLoan, notificationOffer} from "./HeaderIcons";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { RootState } from "../../store/store";
+import {
+  headerNotificationBellIcon,
+  notificationCredit,
+  notificationDebit,
+  notificationFailed,
+  notificationGrant,
+  notificationLoan,
+  notificationOffer,
+} from "./HeaderIcons";
 
-const icons = {
-    "grant" : notificationGrant,
-    "loan" : notificationLoan,
-    "offer" : notificationOffer,
-    "credit" : notificationCredit,
-    "debit" : notificationDebit,
-    "failed" : notificationFailed
+const icons: { [key: string]: string } = {
+  grant: notificationGrant,
+  loan: notificationLoan,
+  offer: notificationOffer,
+  credit: notificationCredit,
+  debit: notificationDebit,
+  failed: notificationFailed,
 };
-const iconsColors = {
-    "credit" : "bg-green-300",
-    "debit" : "bg-red-300",
-    "offer" : "bg-purple-300",
-    "failed" : "bg-red-400",
-    "loan" : "bg-yellow-400",
-    "grant" : "bg-green-400"
+const iconsColors: { [key: string]: string } = {
+  credit: "bg-green-300",
+  debit: "bg-red-300",
+  offer: "bg-purple-300",
+  failed: "bg-red-400",
+  loan: "bg-yellow-400",
+  grant: "bg-green-400",
 };
 
-const BellIcon : React.FC = () => {
-    const [showNotifications, setShowNotifications] = useState(false);
-    const [notifications, setNotifications] = useState([]);
-    const navigate = useNavigate();
-    const userData = useSelector((state:RootState) => state?.user?.userData);
+const BellIcon: React.FC = () => {
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [notifications, setNotifications] = useState<any[]>([]);
+  const navigate = useNavigate();
+  const userData = useSelector((state: RootState) => state?.user?.userData);
 
-    const handleNotificationClick = () => {
-        setShowNotifications(!showNotifications);
-    };
+  const handleNotificationClick = () => {
+    setShowNotifications(!showNotifications);
+  };
 
-    const fetchNotifications = async() => {
-         fetch("/api/notifications/" + userData?.accountNumber + "?limit=5")
-         .then((res) => res.json())
-         .then((data) => {
-            setNotifications(data?.notifications);
-         })
-         .catch(err => console.log(err));
-    };
-    
-    useEffect(() => {
-          fetchNotifications();
-    },[]);
+  const fetchNotifications = async () => {
+    fetch("/api/notifications/" + userData?.accountNumber + "?limit=5")
+      .then((res) => res.json())
+      .then((data) => {
+        setNotifications(data?.notifications);
+      })
+      .catch((err) => console.log(err));
+  };
 
-     return (
-        <div className="relative" onClick={handleNotificationClick}>
-            <div className="rounded-full p-2 bg-lightgrey hover:bg-slate-300">
-                <img
-                src={headerNotificationBellIcon}
-                alt="notification bell icon"
-                className="w-4 h-4 text-gray-600 cursor-pointer hover:text-gray-800"
-                />
-            </div>
-            {showNotifications && (
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  return (
+    <div className="relative" onClick={handleNotificationClick}>
+      <div className="rounded-full p-2 bg-lightgrey hover:bg-slate-300">
+        <img
+          src={headerNotificationBellIcon}
+          alt="notification bell icon"
+          className="w-4 h-4 text-gray-600 cursor-pointer hover:text-gray-800"
+        />
+      </div>
+      {showNotifications && (
+        <div className="absolute right-0 mt-2 w-64 bg-white border border-gray-200 rounded shadow-lg">
+          <ul className="p-2">
+            {notifications.map((notification, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-gray-100 flex gap-2 items-center"
+              >
                 <div
-                    className="absolute right-0 mt-2 w-64 bg-white border border-gray-200 rounded shadow-lg"
+                  className={`w-8 h-8 flex items-center justify-center rounded-full p-1 ${
+                    iconsColors[notification?.type]
+                  }`}
                 >
-                    <ul className="p-2">
-                        {notifications.map((notification, index) => (
-                            <li key={index} className="p-2 hover:bg-gray-100 flex gap-2 items-center">
-                                <div className={`w-8 h-8 flex items-center justify-center rounded-full p-1 ${iconsColors[notification?.type]}`}>
-                                    <img src={icons[notification?.type]} alt={notification?.type} className=""/>
-                                </div>
-                                <div>
-                                    <h4 className="text-[#343C6A] font-semibold text-[13px]">{notification?.type.toUpperCase()}</h4>
-                                    <p className="text-[12px]">{notification?.message.slice(0,26)}{notification?.message.length > 26 && "..."}</p>
-                                </div>
-                            </li>
-                        ))}
-                        <li
-                            className="p-2 text-center text-blue-500 hover:bg-gray-100 cursor-pointer"
-                            onClick={() => navigate("/notifications")}
-                        >
-                            Show all notifications
-                        </li>
-                    </ul>
+                  <img
+                    src={icons[notification?.type]}
+                    alt={notification?.type}
+                    className=""
+                  />
                 </div>
-            )}
+                <div>
+                  <h4 className="text-[#343C6A] font-semibold text-[13px]">
+                    {notification?.type.toUpperCase()}
+                  </h4>
+                  <p className="text-[12px]">
+                    {notification?.message.slice(0, 26)}
+                    {notification?.message.length > 26 && "..."}
+                  </p>
+                </div>
+              </li>
+            ))}
+            <li
+              className="p-2 text-center text-blue-500 hover:bg-gray-100 cursor-pointer"
+              onClick={() => navigate("/notifications")}
+            >
+              Show all notifications
+            </li>
+          </ul>
         </div>
-     )
-}
+      )}
+    </div>
+  );
+};
 
 export default BellIcon;
--- frontend/src/components/Settings/Security.tsx
+++ frontend/src/components/Settings/Security.tsx
@@ -1,88 +1,117 @@
-import React, {useState} from "react";
 import axios from "axios";
+import React, { useState } from "react";
 
-const Security: React.FC<{userData : object | null}> = ({userData}) => {
-    const [formData, setFormData] = useState({
-       enabletwoFactorAuthentication : false,
-       currentPassword : '',
-       newPassword : '' 
-    })
-  
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-      const { name, value } = e.target;
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]:  value
-      }));
-    };
-  
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-  
-      try {
-        const response = await axios.put(`/api/users/password/${userData?.user?.accountNumber}`, {currentPassword: formData.currentPassword, newPassword: formData.newPassword});
-        if (response.status === 200) {
-          alert(response.data.message);
-        }
-      } catch (error) {
-        console.error("Error updating : ", error);
-        alert(response.message);
-      }
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <h3 className="text-[#333B69] text-md font-semibold mb-4">Two-factor Authentication</h3>
-          <div className="flex items-center mb-4">
-            <label className="inline-flex items-center cursor-pointer" htmlFor='enabletwoFactorAuthentication'>
-                <input 
-                  type="checkbox" 
-                  id="enabletwoFactorAuthentication" 
-                  name="enabletwoFactorAuthentication" 
-                  checked={formData.enabletwoFactorAuthentication} 
-                  disabled
-                  className="sr-only peer"/>
-                <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:peer-focus:ring-[#16DBCC] dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#16DBCC]"></div>
-                <span className="ms-3 text-sm font-medium text-[#232323] dark:text-gray-300">Enable or disable two factor authentication</span>
-            </label>
-          </div>
-        </div>
-        <div className="mb-4 w-full md:w-3/6">
-          <label className="block text-[#333B69] text-sm font-semibold mb-2" htmlFor="currentPassword">
-            Current Password
-          </label>
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-md placeholder:text-[#718EBF]"
-            id="currentPassword"
-            name="currentPassword"
-            type="text"
-            value={formData.currentPassword}
-            onChange={handleChange}
-            placeholder="********"
-            required
-          />
-        </div>
-        <div className="mb-4 w-full md:w-3/6">
-          <label className="block text-[#333B69] text-sm font-semibold mb-2" htmlFor="newPassword">
-            New Password
-          </label>
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-md placeholder:text-[#718EBF]"
-            id="newPassword"
-            name="newPassword"
-            value={formData.newPassword}
-            onChange={handleChange}
-            type="text"
-            placeholder="********"
-            required
-          />
-        </div>
-        <div className="w-full flex md:justify-end">
-              <button type="submit" className="w-full md:w-32 px-4 py-2 bg-[#1814F3] text-white rounded-lg">Save</button>
-        </div>
-      </form>
-    );
+const Security: React.FC<{ userData: { [key: string]: any } | null }> = ({
+  userData,
+}) => {
+  const [formData, setFormData] = useState({
+    enabletwoFactorAuthentication: false,
+    currentPassword: "",
+    newPassword: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   };
-  
-  export default Security;
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.put(
+        `/api/users/password/${userData?.user?.accountNumber}`,
+        {
+          currentPassword: formData.currentPassword,
+          newPassword: formData.newPassword,
+        }
+      );
+      if (response.status === 200) {
+        alert(response.data.message);
+      }
+    } catch (error: any) {
+      console.error("Error updating : ", error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <h3 className="text-[#333B69] text-md font-semibold mb-4">
+          Two-factor Authentication
+        </h3>
+        <div className="flex items-center mb-4">
+          <label
+            className="inline-flex items-center cursor-pointer"
+            htmlFor="enabletwoFactorAuthentication"
+          >
+            <input
+              type="checkbox"
+              id="enabletwoFactorAuthentication"
+              name="enabletwoFactorAuthentication"
+              checked={formData.enabletwoFactorAuthentication}
+              disabled
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 dark:peer-focus:ring-[#16DBCC] dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#16DBCC]"></div>
+            <span className="ms-3 text-sm font-medium text-[#232323] dark:text-gray-300">
+              Enable or disable two factor authentication
+            </span>
+          </label>
+        </div>
+      </div>
+      <div className="mb-4 w-full md:w-3/6">
+        <label
+          className="block text-[#333B69] text-sm font-semibold mb-2"
+          htmlFor="currentPassword"
+        >
+          Current Password
+        </label>
+        <input
+          className="w-full px-3 py-2 border border-gray-300 rounded-md placeholder:text-[#718EBF]"
+          id="currentPassword"
+          name="currentPassword"
+          type="text"
+          value={formData.currentPassword}
+          onChange={handleChange}
+          placeholder="********"
+          required
+        />
+      </div>
+      <div className="mb-4 w-full md:w-3/6">
+        <label
+          className="block text-[#333B69] text-sm font-semibold mb-2"
+          htmlFor="newPassword"
+        >
+          New Password
+        </label>
+        <input
+          className="w-full px-3 py-2 border border-gray-300 rounded-md placeholder:text-[#718EBF]"
+          id="newPassword"
+          name="newPassword"
+          value={formData.newPassword}
+          onChange={handleChange}
+          type="text"
+          placeholder="********"
+          required
+        />
+      </div>
+      <div className="w-full flex md:justify-end">
+        <button
+          type="submit"
+          className="w-full md:w-32 px-4 py-2 bg-[#1814F3] text-white rounded-lg"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Security;
