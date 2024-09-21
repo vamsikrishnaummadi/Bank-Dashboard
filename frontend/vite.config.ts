ed4ad806ad53e17756247406d4544adf061825db
Dasari Prashanth <prashanth60238@gmail.com>
2024-09-21 11:40:07 +0530

    vite config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const isDevelopment = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5100',
+      "/api": {
+        target: isDevelopment ? "http://localhost:5100" : "",
         changeOrigin: true,
-        secure: false,
+        secure: !isDevelopment,
       },
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/bankDash.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "assets/bankDash.css";
+          }
+          return "assets/[name][extname]";
+        },
+        manualChunks: undefined,
+      },
+    },
+    cssCodeSplit: false,
   },
 });
